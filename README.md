99fd3708c735d0e8e8f9e095ee262d40b06aad5c
minsm0618 <su960618@gmail.com>
2022-05-01 22:21:30 +0900

    [2022-05-01] Babel 설치

--- README.md
+++
@@ -1,2 +0,0 @@
-# TodayStar
-Apollo Server와 React의 콜라보로 만들어진 미니 SNS 서비스입니다
---
+++ server.js
@@ -0,0 +1,45 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Movie {
+    title: String
+    year: Int
+  }
+
+  type Query {
+    movies: [Movie]
+    movie: Movie
+  }
+
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
+  }
+`;
+
+const resolvers = {
+  Query: {
+    movies: () => [],
+    movie: () => ({ title: "firstMovie", year: 2022 }),
+  },
+
+  Mutation: {
+    createMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
+    deleteMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server
+  .listen()
+  .then(() => console.log("Server is running on http://127.0.0.1:4000/"));

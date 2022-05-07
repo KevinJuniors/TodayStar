be16b0eeee3dd5e678bf428ba5e736cc8d753590
minsm0618 <su960618@gmail.com>
2022-05-07 22:54:45 +0900

    [2022-05-07 22:52] Hashing 작업 및 bcrypt Import

--- server.js
+++ server.js
@@ -2,14 +2,10 @@ require("dotenv").config();
 import { ApolloServer } from "apollo-server";
 import schema from "./schema";
 
-const server = new ApolloServer({
-  schema,
-});
-
 const PORT = process.env.PORT;
+const server = new ApolloServer({ schema, });
 
-server
-  .listen(PORT)
-  .then(() =>
-    console.log(`🎊 Server is running on http://127.0.0.1:${PORT} 🎊`)
-  );
+
+server.listen(PORT).then(
+    () => console.log(`🎆Server is running on http://127.0.0.1:${PORT} 🎇`)
+);
--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,4 +1,3 @@
-import bcrypt from "bcrypt";
 import client from "../client";
 
 export default {
@@ -7,25 +6,17 @@ export default {
       _,
       { firstName, lastName, username, email, password }
     ) => {
+      // check if username or email are already on DB.
       const existingUser = await client.user.findFirst({
         where: {
           OR: [
-            { username }, { email },
+            { username, }, { email, },
           ],
         },
       });
-
       console.log(existingUser);
-      const uglyPassword = await bcrypt.hash(password, 10);
-        return client.user.create({
-            data: {
-                username,
-                email,
-                firstName,
-                lastName,
-                password: uglyPassword,
-            },
-        });
+      // hash password
+      // save and return the user
     },
   },
 };
